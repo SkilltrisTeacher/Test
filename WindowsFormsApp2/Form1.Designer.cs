e8acb055e20c6f00e177de50a99e85b65cabcc7a
SkilltrisTeacher <info@skilltris.ru>
2024-01-23 13:55:07 +0500

    Добавьте файлы проекта.

---
+++ WindowsFormsApp2/Form1.Designer.cs
@@ -0,0 +1,280 @@
+﻿
+namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.combooutput = new System.Windows.Forms.ComboBox();
+            this.comboinput = new System.Windows.Forms.ComboBox();
+            this.InputData = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.outputText = new System.Windows.Forms.TextBox();
+            this.OutputData = new System.Windows.Forms.TextBox();
+            this.menuStrip2 = new System.Windows.Forms.MenuStrip();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripComboBox1 = new System.Windows.Forms.ToolStripComboBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.contextMenuStrip2 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.menuStrip2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.combooutput);
+            this.groupBox1.Controls.Add(this.comboinput);
+            this.groupBox1.Controls.Add(this.InputData);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.groupBox1.Location = new System.Drawing.Point(38, 52);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(369, 491);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Входные данные";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(109, 285);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(100, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Выходные данные";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(109, 221);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(92, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Входные данные";
+            // 
+            // combooutput
+            // 
+            this.combooutput.FormattingEnabled = true;
+            this.combooutput.Items.AddRange(new object[] {
+            "Милеграммы ",
+            "Граммы",
+            "Килограммы",
+            "Центнеры",
+            "Тонны",
+            "Бит",
+            "Килобайт",
+            "Мегабайт",
+            "Гигабайт",
+            "Терабайт"});
+            this.combooutput.Location = new System.Drawing.Point(112, 301);
+            this.combooutput.Name = "combooutput";
+            this.combooutput.Size = new System.Drawing.Size(121, 21);
+            this.combooutput.TabIndex = 2;
+            // 
+            // comboinput
+            // 
+            this.comboinput.FormattingEnabled = true;
+            this.comboinput.Items.AddRange(new object[] {
+            "Милеграммы ",
+            "Граммы",
+            "Килограммы",
+            "Центнеры",
+            "Тонны",
+            "Бит",
+            "Килобайт",
+            "Мегабайт",
+            "Гигабайт",
+            "Терабайт"});
+            this.comboinput.Location = new System.Drawing.Point(112, 237);
+            this.comboinput.Name = "comboinput";
+            this.comboinput.Size = new System.Drawing.Size(121, 21);
+            this.comboinput.TabIndex = 1;
+            // 
+            // InputData
+            // 
+            this.InputData.BackColor = System.Drawing.SystemColors.Control;
+            this.InputData.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.InputData.Font = new System.Drawing.Font("Microsoft Sans Serif", 35F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.InputData.Location = new System.Drawing.Point(12, 110);
+            this.InputData.Name = "InputData";
+            this.InputData.Size = new System.Drawing.Size(357, 60);
+            this.InputData.TabIndex = 0;
+            this.InputData.Text = "56";
+            this.InputData.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(38, 442);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(750, 95);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Перевести";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.button1.Resize += new System.EventHandler(this.button1_Resize);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.button2);
+            this.groupBox2.Controls.Add(this.outputText);
+            this.groupBox2.Controls.Add(this.OutputData);
+            this.groupBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.groupBox2.Location = new System.Drawing.Point(413, 52);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(375, 393);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Выходные данные";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(237, 9);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(138, 38);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Тёмная тема";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // outputText
+            // 
+            this.outputText.BackColor = System.Drawing.SystemColors.Control;
+            this.outputText.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.outputText.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.outputText.Location = new System.Drawing.Point(6, 235);
+            this.outputText.Name = "outputText";
+            this.outputText.Size = new System.Drawing.Size(357, 23);
+            this.outputText.TabIndex = 2;
+            this.outputText.Text = "Килограмм";
+            this.outputText.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // OutputData
+            // 
+            this.OutputData.BackColor = System.Drawing.SystemColors.Control;
+            this.OutputData.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.OutputData.Font = new System.Drawing.Font("Microsoft Sans Serif", 50F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.OutputData.Location = new System.Drawing.Point(6, 132);
+            this.OutputData.Name = "OutputData";
+            this.OutputData.Size = new System.Drawing.Size(357, 76);
+            this.OutputData.TabIndex = 1;
+            this.OutputData.Text = "56";
+            this.OutputData.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // menuStrip2
+            // 
+            this.menuStrip2.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem1,
+            this.toolStripMenuItem2,
+            this.toolStripComboBox1});
+            this.menuStrip2.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip2.Name = "menuStrip2";
+            this.menuStrip2.Size = new System.Drawing.Size(810, 27);
+            this.menuStrip2.TabIndex = 3;
+            this.menuStrip2.Text = "menuStrip2";
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(94, 23);
+            this.toolStripMenuItem1.Text = "О программе";
+            // 
+            // toolStripMenuItem2
+            // 
+            this.toolStripMenuItem2.Name = "toolStripMenuItem2";
+            this.toolStripMenuItem2.Size = new System.Drawing.Size(93, 23);
+            this.toolStripMenuItem2.Text = "Оформление";
+            // 
+            // toolStripComboBox1
+            // 
+            this.toolStripComboBox1.Name = "toolStripComboBox1";
+            this.toolStripComboBox1.Size = new System.Drawing.Size(121, 23);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // contextMenuStrip2
+            // 
+            this.contextMenuStrip2.Name = "contextMenuStrip2";
+            this.contextMenuStrip2.Size = new System.Drawing.Size(61, 4);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(810, 542);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.menuStrip2);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.MinimumSize = new System.Drawing.Size(500, 400);
+            this.Name = "Form1";
+            this.Text = "Конвертер";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.menuStrip2.ResumeLayout(false);
+            this.menuStrip2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox combooutput;
+        private System.Windows.Forms.ComboBox comboinput;
+        private System.Windows.Forms.TextBox InputData;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox outputText;
+        private System.Windows.Forms.TextBox OutputData;
+        private System.Windows.Forms.MenuStrip menuStrip2;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem2;
+        private System.Windows.Forms.ToolStripComboBox toolStripComboBox1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip2;
+    }
+}
+
---
+++ WindowsFormsApp2/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+     
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int inputDataVal = 0;
+            //int outputDatal = Int32.Parse(outputData.Text);
+            object ComboInputVal = comboinput.SelectedItem;
+            object ComboOutputVal = combooutput.SelectedItem;
+            bool error = false;
+            if(ComboInputVal == null)
+            { 
+            //if(ComboInputVal.ToString() == "Не выбрано!")
+            
+                MessageBox.Show("Не выбрано входное значение данных!", "Ошибка!");
+                error = true;
+                return;
+            }
+
+            if(ComboOutputVal == null)
+            {
+                MessageBox.Show("Не выбрано входное значение данных!", "Ошибка!");
+                error = true;
+            }
+            if(InputData.Text.Length > 0)
+            {
+                inputDataVal = Int32.Parse(InputData.Text);
+            }
+            else
+            {
+                InputData.Text = "0";
+            }
+            if (!error)
+            {
+                Console.WriteLine(inputDataVal);
+                Console.WriteLine(ComboInputVal.ToString());
+                Console.WriteLine(ComboOutputVal.ToString());
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            comboinput.Items.Add("Выберите ");
+            combooutput.Items.Add("Выберите ");
+            comboinput.SelectedText = "Выберите";
+
+        }
+
+        private void button1_Resize(object sender, EventArgs e)
+        {
+           int HeightWindow = this.Size.Height;
+           int WidthWindow = this.Size.Width;
+
+            
+            groupBox1.Size = new System.Drawing.Size(WidthWindow / 2 - 55, HeightWindow - button1.Size.Height - 81);
+
+            groupBox2.Size = new System.Drawing.Size(WidthWindow / 2 - 55, HeightWindow - button1.Size.Height - 81);
+
+            groupBox2.Location = new Point(WidthWindow / 2, 52);
+
+            button1.Size = new System.Drawing.Size(WidthWindow - 55,button1.Size.Height);
+
+            button1.Location = new Point(38, HeightWindow - button1.Size.Height - 70);
+
+            OutputData.Size = new System.Drawing.Size(groupBox2.Size.Width - 24, OutputData.Size.Height);
+
+            OutputData.Location = new Point(12, groupBox2.Size.Height / 2 - OutputData.Size.Height / 2);
+
+            outputText.Size = new System.Drawing.Size(groupBox1.Size.Width - 24,outputText.Size.Height);
+
+            outputText.Location = new Point(12, groupBox2.Size.Height / 2 - outputText.Size.Height / 2 + OutputData.Size.Height - 25);
+
+        }
+    }
+}
